8d6a6f3032d315f2e2ec5775038450937c0ae5a9
RaRAshed <rnrashedrn@gmail.com>
2024-07-13 10:21:17 -0700

    nabvar system added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nabvar from './components/Navbar/Nabvar';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Nabvar />
+
+    </div>
+  );
+};
+
+export default App;
